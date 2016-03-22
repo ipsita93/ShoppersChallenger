669e0c38d353229d271bd9a59d1db4210ad51916
ipsita_mohapatra <ipsitamohapatra93@gmail.com>
2016-03-22 22:16:01 +0800

    Add alter table commands.

--- Shoppers/src/Parser.java
+++ Shoppers/src/Parser.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Parser {
@@ -13,12 +14,13 @@ public class Parser {
 		String trainHistory_filename = "C:\\Users\\Sony\\Google Drive\\NUS\\8 - Year 4 Sem 2\\CS4225 Massive Data Processing Techniques in Data Science\\Project\\ShoppersChallenge\\trainHistory\\trainHistory.csv";
 		// String testHistory_filename = "./testHistory/testHistory.csv";
 
-		populateOffersTable(offers_filename, "offers");
-		// populateTransactionTable(transactions_filename, "transactions");
-		populateTrainHistoryTable(trainHistory_filename, "trainHistory");
-		populateCustomersTable();
+		// populateOffersTable(offers_filename, "offers");
+		// populateTrainHistoryTable(trainHistory_filename, "trainHistory");
+		// populateCustomersTable();
 		populateNewFeaturesCustomersTable(transactions_filename); // takes in
 																	// filename
+
+		// populateTransactionTable(transactions_filename, "transactions");
 
 		System.out.println("Parsing done.");
 	}
@@ -44,24 +46,33 @@ public class Parser {
 				// Create the file reader
 				fileReader = new BufferedReader(new FileReader(fileToParse));
 
+				String sql = "alter table customers add column has_bought_company_before_q int;";
+				stmt.executeUpdate(sql);
+				
 				int numLines = 0;
 				// Read the file line by line
 				while ((line = fileReader.readLine()) != null) {
 					if (numLines != 0) {
 						// Get all tokens available in line
 						String[] tokens = line.split(DELIMITER);
-						for (int i = 0; i < tokens.length; i++) {
-							String sql = "";
-
-
-							
-							
-							
-							
-							System.out.println(sql);
-							stmt.executeUpdate(sql);
-
+						// for (int i = 0; i < tokens.length; i++) {
+						String query = "select c.has_bought_company_before_q from customers c"
+								+ " where c.customer_id = "
+								+ tokens[0]
+								+ " and c.company_id = " + tokens[3] + ";";
+						ResultSet rs = stmt.executeQuery(query);
+						int iVal1 = 0;
+						while (rs.next()) {
+							iVal1 = Integer.getInteger(rs
+									.getString("has_bought_company_before_q"));
 						}
+						iVal1++;
+						sql = "update customers c "
+								+ "set c.has_bought_company_before_q = " + iVal1
+								+ " where c.customer_id = " + tokens[0] + ";";
+						stmt.executeUpdate(sql);
+
+						// }
 
 					}
 					numLines++;
